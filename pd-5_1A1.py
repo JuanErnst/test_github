5aa4f9fc8469cd6ca8d354766ee4d0f60f30fdcc
JuanErnst <128844151+JuanErnst@users.noreply.github.com>
2023-07-20 12:58:35 -0300

    Add files via upload

---
+++ pd-5_1A1.py
@@ -0,0 +1,11 @@
+
+# Alumno: Forkear el siguiente repositorio de Github de inoveAlumnos:
+# https://github.com/InoveAlumnos/test_github
+
+"https://github.com/JuanErnst/django-intro/blob/main/pd-5_1A1.py"
+
+# Crear una variable llamada "mi_repositorio" y en ella
+# almacene la URL o link de su repositorio forkeado
+# ejemplo: 
+# mi_repositorio = https://github.com/miusuario/test_github
+
